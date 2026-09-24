8f6627828915130264ad201394bf036772a9d321
Kepa Gogenola <kepa.gogenola@alumnos.uai.edu.ar>
2026-09-24 20:21:42 -0300

    Agregue mejoras para el modal a distintos dispositivos y la posibilidad de ver imagenes desde cualquier dispositivo

--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
 require("dotenv").config();
-const { poolPromise } = require("./config/database");
-const app = require("./app");
 
-const PORT = process.env.PORT || 4000;
+const {
+  poolPromise,
+} = require(
+  "./config/database"
+);
 
-app.listen(PORT, () => {
-  console.log(`Servidor HostFlow ejecutándose en http://localhost:${PORT}`);
-});
+const app = require(
+  "./app"
+);
+
+const PORT =
+  process.env.PORT ||
+  4000;
+
+const HOST =
+  process.env.HOST ||
+  "0.0.0.0";
+
+app.listen(
+  PORT,
+  HOST,
+  () => {
+    console.log(
+      `Servidor HostFlow ejecutándose en el puerto ${PORT}`
+    );
+
+    console.log(
+      `Acceso local: http://localhost:${PORT}`
+    );
+  }
+);
